a64fe9794be3f703ef6f6522c9edc850fe33d561
AR-Rashik <66801782+AR-Rashik@users.noreply.github.com>
2022-11-26 02:26:23 +0600

    mongoDB setup

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// mongodb
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://<username>:<password>@cluster0.j6uwcgb.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 app.get("/", (req, res) => {
   res.send("Book sell server is running");
 });
